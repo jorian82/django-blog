1940c2ee6509dd8d2e5ecba3815caa1e53f8e742
Jorge Rivera <jorge.rivera@ssde.com.mx>
2018-02-22 17:22:48 -0600

    Added configuration to deploy to heroku and add postgresql database

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 #import django_heroku
