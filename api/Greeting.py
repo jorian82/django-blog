25746373ef8b65e763bbfb754166e41b1efd4d64
Jorge Rivera <jorge.rivera@ssde.com.mx>
2018-02-23 22:25:47 -0600

    remove not used files

--- api/Greeting.py
+++
@@ -1,6 +0,0 @@
-from .Utils import Object
-
-class Greeting(Object):
-    def __init__(self, name="Stranger"):
-        self.name = name
-        self.message = "Hello {0}!!! How are you?".format(name)
